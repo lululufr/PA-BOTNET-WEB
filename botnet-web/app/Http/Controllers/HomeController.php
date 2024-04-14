7e9dd5d1ac9fd5fe6a7d43b4dd15b5bb630d6cae
Jiullian Ferreira <jiullian77470@gmail.com>
2024-04-14 16:00:35 +0200

    fix - on option

--- botnet-web/app/Http/Controllers/HomeController.php
+++ botnet-web/app/Http/Controllers/HomeController.php
@@ -47,9 +47,9 @@ class HomeController extends Controller
     {
 
         // Exécute la commande ipconfig et récupère le résultat dans $output
-        system('/home/debian/PA-BOTNET-PYSRV/botnet-start.sh', $output, $return);
+        system('/home/debian/PA-BOTNET-PYSRV/botnet-start.sh', $output);
 
-        system('ps -a', $output, $return);
+        system('ps -a', $output);
 
         // Convertit le tableau $output en une chaîne de caractères
         $outputString = implode("\n", $output);
