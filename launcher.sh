d680d13b0f6c6221d0fc41a5e3af045ffd458d04
lululufr <lucas.lm.miller@gmail.com>
2024-06-22 17:18:47 +0200

    hotfix

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-php /home/lucas/Documents/PA-BOTNET-WEB/botnet-web migrate
-php /home/lucas/Documents/PA-BOTNET-WEB/botnet-web db:seed
-php /home/lucas/Documents/PA-BOTNET-WEB/botnet-web/artisan serv --host 0.0.0.0 --port 8080
+php /home/debian/PA-BOTNET-WEB/botnet-web migrate
+php /home/debian/PA-BOTNET-WEB/botnet-web db:seed
+php /home/debian/PA-BOTNET-WEB/botnet-web/artisan serv --host 0.0.0.0 --port 8080
 
