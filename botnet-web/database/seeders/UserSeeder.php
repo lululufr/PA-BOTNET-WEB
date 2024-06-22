22151e7b5159f3128394d67f3a7f64cb8e3445cf
lululufr <lucas.lm.miller@gmail.com>
2024-06-22 15:02:56 +0200

    Add proper launcher

--- botnet-web/database/seeders/UserSeeder.php
+++ botnet-web/database/seeders/UserSeeder.php
@@ -5,14 +5,20 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
+
     public function run(): void
     {
+
+        //si l'utilisateur serviceuser existe alors on le supprime
+        if (Schema::hasTable('users')) {
+            User::where('email', 'serviceuser@bot.net')->delete();
+        }
+
         $user = new User();
         $user->lastname = 'bot';
         $user->firstname = 'net';
--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 php /home/lucas/Documents/PA-BOTNET-WEB/botnet-web migrate
-php /home/lucas/Documents/PA-BOTNET-WEB/botnet-web seed
+php /home/lucas/Documents/PA-BOTNET-WEB/botnet-web db:seed
 php /home/lucas/Documents/PA-BOTNET-WEB/botnet-web/artisan serv --host 0.0.0.0 --port 8080
 
